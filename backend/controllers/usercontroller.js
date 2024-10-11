dd93d807132a7584c2a60b613ccbac8fd480bb41
hi <hi@his-MacBook-Pro.local>
2024-10-11 02:05:33 -0700

    added get a user controller and routes

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -3,46 +3,42 @@ const Transaction = require('../models/Transactions');
 const asyncHandler = require('express-async-handler');
 
 
+//Get a User
 
+const getUserAccount = asyncHandler(async (req, res) => {
+  const account = await Account.findOne({ user: req.user.id }).populate('transactions');
+  res.json(account);
+});
 
-const getUserAccount = async (req, res) => {
-  try {
-    const account = await Account.findOne({ user: req.user.id }).populate('transactions');
-    res.json(account);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+//Make Transaction
 
-makeTransaction = async (req, res) => {
+const makeTransaction = asyncHandler(async (req, res) => {
   const { amount, type } = req.body;
-  try {
-    let account = await Account.findOne({ user: req.user.id });
-    if (type === 'debit' && account.balance < amount) {
-      return res.status(400).json({ msg: 'Insufficient funds' });
-    }
-
-    const newTransaction = new Transaction({
-      amount,
-      type,
-      account: account._id,
-    });
-
-    await newTransaction.save();
-    account.transactions.push(newTransaction._id);
-
-    if (type === 'credit') {
-      account.balance += amount;
-    } else {
-      account.balance -= amount;
-    }
-
-    await account.save();
-    res.json({ msg: 'Transaction successful', account });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  
+  let account = await Account.findOne({ user: req.user.id });
+  
+  if (type === 'debit' && account.balance < amount) {
+    return res.status(400).json({ msg: 'Insufficient funds' });
   }
-};
+
+  const newTransaction = new Transaction({
+    amount,
+    type,
+    account: account._id,
+  });
+
+  await newTransaction.save();
+  account.transactions.push(newTransaction._id);
+
+  if (type === 'credit') {
+    account.balance += amount;
+  } else {
+    account.balance -= amount;
+  }
+
+  await account.save();
+  res.json({ msg: 'Transaction successful', account });
+});
 
 
 module.exports = { getUserAccount, makeTransaction };
